eac79f41cda6d5e6618791c68dd74d4bf5824476
g0urav-hustler <gouravchouhan966@gmail.com>
2023-03-24 13:09:58 +0530

    create and run setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = "src",
+    version = "0.0.1",
+    description = "It is a wine quality project",
+    author = "Gourav_Chouhan",
+    packages = find_packages(),
+    license = "MIT",
+    
+)
